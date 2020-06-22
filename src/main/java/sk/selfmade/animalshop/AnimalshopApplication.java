0c8113d3f901feccad346a0a0b4d7066546a69aa
selfmadedigital <3jNfgDSf8@>
2020-06-23 01:14:54 +0200

    config changes

--- src/main/java/sk/selfmade/animalshop/AnimalshopApplication.java
+++ src/main/java/sk/selfmade/animalshop/AnimalshopApplication.java
@@ -4,11 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AnimalshopApplication {
+public class AnimalshopApplication extends SpringBootServletInitializer {
 	
 	Logger logger = LoggerFactory.getLogger(AnimalshopApplication.class);
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+	    return builder.sources(AnimalshopApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimalshopApplication.class, args);
